9d80a77022c7a2d02bba9faa4865f170eb36383d
JavieraCastroA <javiera.castro@ug.uchile.cl>
2017-06-12 14:23:15 -0400

    Add files via upload

---
+++ header.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+
+    <!-- COMMON TAGS -->
+<meta charset="utf-8">
+<title>La imagen fotográfica como expresión de la muerte fuera de su registro y su incidencia en la construcción de la memoria</title>
+<!-- Search Engine -->
+<meta name="description" content="Investigación Seminario de Diseño Gráfico de la Universidad de Chile en torno a la fotografía, la muerte, y la construcción de la memoria tanto individual como colectiva">
+<meta name="image" content="background-image: url('img/home-bg.png')">
+<!-- Schema.org for Google -->
+<meta itemprop="name" content="La imagen fotográfica como expresión de la muerte fuera de su registro y su incidencia en la construcción de la memoria">
+<meta itemprop="description" content="Investigación Seminario de Diseño Gráfico de la Universidad de Chile en torno a la fotografía, la muerte, y la construcción de la memoria tanto individual como colectiva">
+<meta itemprop="image" content="background-image: url('img/home-bg.png')">
+<!-- Twitter -->
+<meta name="twitter:card" content="summary">
+<meta name="twitter:title" content="La imagen fotográfica como expresión de la muerte fuera de su registro y su incidencia en la construcción de la memoria">
+<meta name="twitter:description" content="Investigación Seminario de Diseño Gráfico de la Universidad de Chile en torno a la fotografía, la muerte, y la construcción de la memoria tanto individual como colectiva">
+<!-- Open Graph general (Facebook, Pinterest & Google+) -->
+<meta name="og:title" content="La imagen fotográfica como expresión de la muerte fuera de su registro y su incidencia en la construcción de la memoria.">
+<meta name="og:description" content="Investigación Seminario de Diseño Gráfico de la Universidad de Chile en torno a la fotografía, la muerte, y la construcción de la memoria tanto individual como colectiva">
+<meta name="og:image" content="background-image: url('img/home-bg.png')">
+<meta name="og:url" content="https://github.com/JavieraCastroA/sitiophp_evaluado">
+<meta name="og:site_name" content="La imagen fotográfica como expresión de la muerte fuera de su registro y su incidencia en la construcción de la memoria">
+<meta name="og:locale" content="es_CL">
+<meta name="og:type" content="website">
+
+
+
+    <title>La imagen melancólica</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Theme CSS -->
+    <link href="css/clean-blog.css" rel="stylesheet">
+
+    <!-- Custom Fonts -->
+    <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:200,200i,300,300i,400,400i,600,600i,700,700i,900,900i" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,400i,700,700i" rel="stylesheet">
+
+
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-custom navbar-fixed-top">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    Menu <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="index.php">Fotografía y muerte</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="index.php">Artículos</a>
+                    </li>
+                    <li>
+                        <a href="biblio.php">Bibliografía</a>
+                    </li>
+                    <li>
+                        <a href="visual.php">Visualizaciones</a>
+                    </li>
+                    <li>
+                        <a href="antecedentes.php">Antecedentes</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <!-- Page Header -->
